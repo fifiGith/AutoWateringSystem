c6d214285d139e6b7612750cd3c425bd6e5b8276
Thatchanon NABANDIT <fordfifi@hotmail.com>
2016-12-02 12:17:26 +0700

    first commit

---
+++ firmware/peri.h
@@ -0,0 +1,7 @@
+#define IS_SWITCH_PRESSED() ((PINC &(1<<PC3))==0)
+
+void init_peripheral();
+void set_led(uint8_t,uint8_t);
+void set_led_value(uint8_t);
+uint16_t read_adc(uint8_t channel);
+uint16_t get_light();
---
+++ java/src/practicum/TestUsb.java
@@ -0,0 +1,19 @@
+package practicum;
+
+import org.usb4java.Device;
+
+public class TestUsb {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		McuBoard.initUsb();
+		Device[] devs = McuBoard.findBoards();
+		McuBoard b = new McuBoard(devs[0]);
+		b.write((byte)0,(short)2,(short)0);
+		b.write((byte)0,(short)1,(short)1);
+		b.write((byte)0,(short)0,(short)1);
+		System.out.println(b.read((byte)2,(short)0,(short)0)[0]);
+		McuBoard.cleanupUsb();
+	}
+
+}
